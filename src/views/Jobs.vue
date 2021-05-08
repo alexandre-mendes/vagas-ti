30b34b83d8a83b24775a90f8fb18ebadc887039d
Clodoaldo Dantas <clodoaldodantas8@gmail.com>
2021-05-08 15:50:28 -0300

    ✨ sidebar component

--- src/views/Jobs.vue
+++ src/views/Jobs.vue
@@ -4,12 +4,13 @@
     <topbar>
       <b-input-group class="search-group">
         <b-form-input
+          @keyup.enter="search"
           v-model="text"
           placeholder="Pesquise pelo cargo ou empresa"
         />
 
         <b-input-group-append>
-          <b-button :disabled="!text">
+          <b-button @click="search">
             <search-icon size="20" />
           </b-button>
         </b-input-group-append>
@@ -22,41 +23,66 @@
     <!-- Main Content -->
     <main id="main-content" v-else>
       <b-container>
+        <div class="filters">
+          <b-link v-b-toggle.filters>
+            Filtrar vagas
+            <filter-icon size="24" />
+          </b-link>
+        </div>
+
         <div class="grid">
-          <job-item v-for="job in jobs" :key="job.id" :job="job" />
+          <job-item v-for="job in jobsFiltered" :key="job.id" :job="job" />
         </div>
       </b-container>
     </main>
+
+    <!-- Sidebar -->
+    <sidebar />
   </div>
 </template>
 
 <script>
-import { SearchIcon } from 'vue-feather-icons';
+import { SearchIcon, FilterIcon } from 'vue-feather-icons';
 import { api } from '@/services/api';
 
 import Topbar from '@/components/Topbar.vue';
 import Loading from '../components/Loading.vue';
 import JobItem from '../components/JobItem.vue';
+import Sidebar from '../components/Sidebar.vue';
 
 export default {
   name: 'Jobs',
   components: {
-    Topbar,
     SearchIcon,
+    FilterIcon,
+    Topbar,
     Loading,
     JobItem,
+    Sidebar,
   },
   data() {
     return {
       text: '',
       loading: true,
       jobs: [],
+      jobsFiltered: [],
     };
+  },
+  methods: {
+    search() {
+      this.jobsFiltered = this.jobs.filter(job => {
+        return (
+          job.title.toLowerCase().includes(this.text) ||
+          job.company.toLowerCase().includes(this.text)
+        );
+      });
+    },
   },
   async mounted() {
     try {
       const response = await api.get('jobs');
       this.jobs = response.data;
+      this.jobsFiltered = response.data;
     } catch (error) {
       console.error(error.message);
     } finally {
@@ -81,7 +107,7 @@ export default {
 }
 
 #main-content {
-  padding: 2.5rem 0;
+  padding-bottom: 2.5rem;
 }
 
 .grid {
@@ -91,6 +117,29 @@ export default {
 
   @media screen and (max-width: 800px) {
     grid-template-columns: 1fr;
+  }
+}
+
+.filters {
+  width: 100%;
+  display: flex;
+  padding: 1.5rem 0;
+  justify-content: flex-end;
+
+  a {
+    display: flex;
+    align-items: center;
+    color: #212529;
+    text-decoration: none;
+    transition: all 0.15s ease-in-out;
+
+    &:hover {
+      color: var(--blue);
+    }
+  }
+
+  svg {
+    margin-left: 0.5rem;
   }
 }
 </style>
